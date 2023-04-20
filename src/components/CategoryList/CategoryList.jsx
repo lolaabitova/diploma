cb527b162fa14d0b8084f1a4982559632fab9b51
Lola <lolaabitova90@gmail.com>
2023-04-20 15:17:17 +0600

    Added "CategoryList" components, which shows the categories and the path to them

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,42 +1,24 @@
-
-import { getDocs } from "firebase/firestore";
-import { useEffect, useState } from "react";
-import { NavLink } from "react-router-dom";
-import { categoryCollection } from "../../firebase";
+import { useContext, useEffect, useState } from "react";
 import "./CategoryList.css";
+import { NavLink } from "react-router-dom";
+import { AppContext } from "../../App";
 
-export default function CategoryList(){
-  const [categories, setCategories] = useState([]);
-  useEffect(() =>{
-    getDocs(categoryCollection)
-    .then(snapshot => {
-      const newCategories = [];
+export default function CategoryList() {
+  const { categories } = useContext(AppContext);
 
-
-      snapshot.docs.forEach(doc => {
-        const category = doc.data();
-        category.id = doc.id;
-
-        newCategories.push(category);
-      });
-
-      setCategories(newCategories);
-    })
-  }, []);
-  
-
-  const output = categories.map(category =>(
-        <li key={category.id}>
-          <NavLink to={"/category/" + category.path}>
-            {category.name}
-          </NavLink>
-        </li>
+  const output = categories.map(category => (
+    <li key={category.id}>
+      <NavLink to={'/category/' + category.path}>
+        {category.name}
+      </NavLink>
+    </li>
   ));
-    return(
-      <div className="CategoryList">
-        <ul>
-          {output}
-        </ul>
-      </div>
-    )
+
+  return (
+    <div className="CategoryList">
+      <ul>
+        {output}
+      </ul>
+    </div>
+  );
 }
