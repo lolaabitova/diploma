5c0d7b24c7a2b6b20f94a705bcbf75fd04724c75
Lola <lolaabitova90@gmail.com>
2023-05-19 14:10:06 +0600

    Created a product page arranged the elements in the right order

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -6,20 +6,47 @@ import AddToCart from "../components/AddToCart/AddToCart";
 
 export default function Product() {
   const { params } = useMatch("/product/:path");
-  const { products } = useContext(AppContext); 
+  const { products } = useContext(AppContext);
 
-  const product = products.find(product => product.path === params.path);
+  const product = products.find((product) => product.path === params.path);
 
   if (!product) {
     return <NotFound />;
   }
 
   return (
-    <div className="Product">
-      <h1>{product.name}</h1>
-      <img src={product.picture} alt={product.name} />
-      <span>{product.price} $</span>
-      <AddToCart product={product} />
+    <div className="Product container">
+      <div class="hover-effect-btn">
+        <img src={product.picture} alt={product.name} />
+        <div className="product-name title">
+          <div>
+            <span>{product.name}</span>
+          </div>
+          <div>
+            <span>{product.price}$</span>
+          </div>
+        </div>
+        <div class="product-overlay"></div>
+        <div class="button">
+          <AddToCart product={product} /> 
+         
+        </div>
+        
+         
+        
+      </div>
+      <div>
+     <scroll-container>
+            <scroll-page id="page-1">
+            <span className="product-year">{product.name}</span> 
+            
+            </scroll-page>
+            <scroll-page id="page-2"><span className="product-year">{product.year}</span></scroll-page>
+            <scroll-page id="page-3"><span className="product-year">{product.description}</span>  </scroll-page>
+            <scroll-page id="page-3"></scroll-page>
+          </scroll-container>
+          
+          </div> 
     </div>
   );
 }
