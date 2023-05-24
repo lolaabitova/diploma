7b4dcd44043a4c198e36d47f4e5980b2aeb887ae
Lola <lolaabitova90@gmail.com>
2023-05-24 19:38:49 +0600

    Changed the map button by adding one more element

--- src/components/CartLink/CartLink.jsx
+++ src/components/CartLink/CartLink.jsx
@@ -7,16 +7,16 @@ export default function CartLink() {
   const { cart } = useContext(AppContext);
 
   // вычислить кол-во товаров
-  const total = Object.values(cart).reduce(
-    (acc, num) => acc + num, 0
-  );
+  const total = Object.values(cart).reduce((acc, num) => acc + num, 0);
 
   return (
     <div className="CartLink">
-      <button class="custom-btn btn-12"><span><NavLink to="/cart">
-        Cart ({total})
-      </NavLink></span><span>CART</span></button>
-      
+      <button class="custom-btn btn-12">
+        <span>
+          <NavLink to="/cart">Cart ({total})</NavLink>
+        </span>
+        <span>CART</span>
+      </button>
     </div>
   );
 }
