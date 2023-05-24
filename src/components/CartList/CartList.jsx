59cf72238b8053e51624cc65984557f3f79ec13a
Lola <lolaabitova90@gmail.com>
2023-05-24 14:46:49 +0600

    Added styles to order forms

--- src/components/CartList/CartList.jsx
+++ src/components/CartList/CartList.jsx
@@ -34,7 +34,7 @@ export default function CartList() {
           value={cart[product.id]}
         />
         <span>{product.price * cart[product.id]} som</span>
-        <button onClick={() => onRemoveClick(product)}>Remove</button>
+        <div className="line-btn line-btn" onClick={() => onRemoveClick(product)}><span>Remove</span></div>
       </div>
     ));
 
