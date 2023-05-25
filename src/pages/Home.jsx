a61448dee6ebfc318e6751d5b497aca40539f05b
Lola <lolaabitova90@gmail.com>
2023-05-25 12:03:20 +0600

    Changed the picture on the main page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,98 @@
+import "./Home.css";
+
 export default function Home() {
   return (
     <div className="Home">
       <div className="home-welcome">
         <div class="home-welcome-img">
           <img
-            src="https://wp-s.ru/wallpapers/2/0/535207168449163/krasnyj-chevrolet-korvet-edushhij-po-doroge.jpg"
+            src="https://avatars.mds.yandex.net/i?id=5d73ce8dd8aa6c665c27b8129a21c14d5e1d157d-9180497-images-thumbs&n=13"
             alt=""
           />
         </div>
         <div class="home-welcome-text">
           <h1>Change your way on our auto</h1>
-           </div>
+        </div>
+      </div>
+      <h1>Our car models</h1>
 
+      <div className="home-photo-categorii">
+        
+        <div className="change-photos">
+          <div class="change-photo">
+            <img
+              src="https://oboi-download.ru/files/wallpapers/1274/28943.jpg"
+              alt=""
+            />
+          </div>
+          <div className="change-photo">
+            <img
+              src="https://oboi-download.ru/files/wallpapers/1251/28744.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="change-photos">
+          <div class="change-photo">
+            <img
+              src="https://media.istockphoto.com/id/458594985/photo/lexus-logo-on-a-new-rx-270-car-grille.jpg?s=612x612&w=0&k=20&c=tqHAvRjXFZUr4oJ9Lga2VdscMvEnKhyF5igZv_wRl8o="
+              alt=""
+            />
+          </div>
+          <div className="change-photo">
+            <img
+              src="https://img.wallscloud.net/uploads/thumb/2102254304/lexus_rx_lexus_mashina_147584_3840x2160-1024x576-MM-80.webp"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="change-photos">
+          <div class="change-photo">
+            <img
+              src="https://vsthemes.org/uploads/posts/2017-12/1582034139_toyota_vsthemes_ru-1.webp"
+              alt=""
+            />
+          </div>
+          <div className="change-photo">
+            <img
+              src="https://phonoteka.org/uploads/posts/2022-09/thumbs/1663881374_12-phonoteka-org-p-kamri-oboi-na-aifon-instagram-12.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="change-photos">
+          <div class="change-photo">
+            <img
+              src="https://i.pinimg.com/originals/c5/5c/3f/c55c3fe16f8fdffb2b506f13cd9a6cd9.png"
+              alt=""
+            />
+          </div>
+          <div className="change-photo">
+            <img
+              src="https://img.mercedes-benz-kiev.com/data/news/181-mercedes-benz-vision-avtr/181-Mercedes-Benz-vision-avtr-1.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="change-photos">
+          <div class="change-photo">
+            <img
+              src="https://cdn.wallpapersafari.com/2/54/OgBVkt.jpg"
+              alt=""
+            />
+          </div>
+          <div className="change-photo">
+            <img
+              src="https://zastavok.net/main/auto/166724892140.jpg"
+              alt=""
+            />
+          </div>
+        </div>
       </div>
       <div class="home-header">
         <div class="home-header-img">
           <img
-            src="https://gamerwall.pro/uploads/posts/2022-03/thumbs/1648681051_7-gamerwall-pro-p-fon-muzhskoi-s-mashinoi-krasivie-8.jpg"
+            src="https://damion.club/uploads/posts/2022-09/1663926489_1-damion-club-p-fon-avtomobilnii-vkontakte-3.jpg"
             alt=""
           />
         </div>
@@ -27,11 +103,6 @@ export default function Home() {
             dealers around the world.
           </h1>
         </div>
-      </div>
-
-      <div>
-        <h2>CURRENT OFFERS</h2>
-        
       </div>
     </div>
   );
